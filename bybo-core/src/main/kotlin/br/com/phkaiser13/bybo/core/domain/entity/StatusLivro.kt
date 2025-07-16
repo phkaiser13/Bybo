d176d78b1bb55243b1b98981f6dfff79e4462f29
phkaiser13 <phgarcia2008@icloud.com>
2025-07-16 12:57:27 -0300

    First Commit

---
+++ bybo-core/src/main/kotlin/br/com/phkaiser13/bybo/core/domain/entity/StatusLivro.kt
@@ -0,0 +1,14 @@
+﻿// 2025
+// By Pedro henrique garcia.
+// Github/gitlab: Phkaiser13
+
+package br.com.phkaiser.bybo.core.domain.entity
+
+/**
+ * Enum para representar o status de um livro na biblioteca.
+ * Escrito em Kotlin para demonstrar a interoperabilidade.
+ */
+enum class StatusLivro {
+    DISPONIVEL,
+    EMPRESTADO
+}
